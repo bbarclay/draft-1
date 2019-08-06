92f58476cea1145ab9514a93d2c826801b3fb4f5
bbarclay <barclaybrandon@hotmail.com>
2019-08-06 09:04:03 -0600

    Pipedream laravel foundation

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(GarageSeeder::class);
-        $this->call(UserSeeder::class);
-        $this->call(CarSeeder::class);
+        // $this->call(UsersTableSeeder::class);
     }
 }
